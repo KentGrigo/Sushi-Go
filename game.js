560e5214692bab966730ce974c16ff44c4ec9e26
Kent Grigo <KentGrigo@hotmail.com>
2024-06-26 21:19:11 +0200

    Make number of players configurable

--- game.js
+++ game.js
@@ -1,16 +1,25 @@
-const numberOfPlayers = 3 // Range: 2 - 5
-const numberOfStartingCards = {
-    2: 10,
-    3: 9,
-    4: 8,
-    5: 7,
-}[numberOfPlayers]
+function loadGame() {
+    document.getElementById("form").submit()
+    const numberOfPlayers = document.getElementById("numberOfPlayers").valueAsNumber
+    if (!Number.isInteger(numberOfPlayers) || numberOfPlayers < 2 || 5 < numberOfPlayers) {
+        console.error("The number of players must be between 2 and 5, inclusive.")
+        return
+    }
 
-const deck = new Deck()
-const players = []
-for (var playerNumber = 0; playerNumber < numberOfPlayers; playerNumber++) {
-    const playerCards = deck.pop(numberOfStartingCards)
-    const player = new Player(playerCards)
-    players.push(player)
+    const numberOfStartingCards = {
+        2: 10,
+        3: 9,
+        4: 8,
+        5: 7,
+    }[numberOfPlayers]
+
+    const deck = new Deck()
+    const players = []
+    for (var playerNumber = 0; playerNumber < numberOfPlayers; playerNumber++) {
+        const playerCards = deck.pop(numberOfStartingCards)
+        const player = new Player(playerCards)
+        players.push(player)
+    }
+    console.log(players)
+
 }
-console.log(players)
